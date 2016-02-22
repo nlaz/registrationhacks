5b2b045ef71e16cfcded4a2ed701128150433b62
nlaz <nikolazaris@utexas.edu>
2016-02-21 19:58:52 -0600

    add user request

--- js/form.js
+++ js/form.js
@@ -1,10 +1,17 @@
 var formFill = function() {
-  var accessToken = getAccessToken();
+  var accessToken = getAccessToken(),
+      request_url = "https://my.mlh.io/api/v1/user";
+
+  request_url += "?access_token=" + accessToken;
+
+  $.get(request_url, function(response, status) {
+    console.log(response.data['email']);
+  });
 };
 
 var getAccessToken = function() {
   var parameters = window.location.href.split(/[&#]/);
-  if (parameters.length > 0)
+  if (parameters.length > 1)
     var access_token = parameters[1].replace('access_token=', '');
 
   console.log(access_token);
