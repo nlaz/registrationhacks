5122d39ec1ca93fc9e60e8c11ac8e06c2de28b82
nlaz <nikolazaris@utexas.edu>
2016-02-21 18:52:20 -0600

    misc js fixes

--- form.js
+++ form.js
@@ -3,10 +3,9 @@ var formFill = function() {
 };
 
 var getAccessToken = function() {
-  var access_token,
-      parameters = window.location.href.split(/[&#]/);
+  var parameters = window.location.href.split(/[&#]/);
   if (parameters.length > 0)
-    access_token = parameters[1].replace('access_token=', '');
+    var access_token = parameters[1].replace('access_token=', '');
 
   console.log(access_token);
   return access_token;
